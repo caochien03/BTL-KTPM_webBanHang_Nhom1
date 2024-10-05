9fc134f94384b71d870adc92b1c694748ea41c7c
caochien <nguyencaochien03@gmail.com>
2024-10-05 17:22:55 +0700

    btl cuối kỳ ktpm

---
+++ frontend/src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import "./footer.scss";
+
+const Footer = () => {
+    return (
+        <footer>
+            <div> Thiết kế &copy; by Nhóm NumberOne</div>
+        </footer>
+    );
+};
+
+export default Footer;
